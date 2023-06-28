2a733c72c809ce8ec5fd8e68dad8ea4c1bf55e32
Daniel Luengo <daniel.luengo.v@gmail.com>
2023-06-28 00:03:08 -0400

    switch

---
+++ 01-introduccion/Dia 2/switch.js
@@ -0,0 +1,17 @@
+var num = parseInt(prompt("Ingrese un numero del 1 al 4"));
+
+switch(num) {
+    case  1:
+        alert("Usted ingreso el numero 1");
+        break;
+    case 2:
+        alert("Usted ingreso el numero 2");
+        break;
+    case 3:
+        alert("Usted ingreso el numero 3");
+        break;
+    case 4:
+        alert("Usted ingreso el numero 4");
+        break;
+        default: alert("No ingreso uno de los numeros pedidos")
+}
