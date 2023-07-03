5eaa182cfacb7147f2828aaf596f16e47a78ccee
Daniel Luengo <daniel.luengo.v@gmail.com>
2023-07-03 00:05:00 -0400

    Switch

--- 01-introduccion/Dia 2/switch.js
+++ 01-introduccion/Dia 2/switch.js
@@ -1,18 +1,77 @@
-var num = parseInt(prompt("Ingrese un numero del 1 al 4"));
+// var num = parseInt(prompt("Ingrese un numero del 1 al 4"));
+
+// switch(num) {
+//     case  1:
+//         alert("Usted ingreso el numero 1");
+//         break;
+//     case 2:
+//         alert("Usted ingreso el numero 2");
+//         break;
+//     case 3:
+//         alert("Usted ingreso el numero 3");
+//         break;
+//     case 4:
+//         alert("Usted ingreso el numero 4");
+//         break;
+//         default: alert("No ingreso uno de los numeros pedidos")
+// }
+   
+var num = prompt("Ingresa un numero del 1 al 7");
 
 switch(num) {
-    case  1:
-        alert("Usted ingreso el numero 1");
+    case "1":
+        alert("El numero 1 correspone al dia Lunes");
         break;
-    case 2:
-        alert("Usted ingreso el numero 2");
+    case "2":
+        alert("El numero 2 corresponde al dia Martes");
         break;
-    case 3:
-        alert("Usted ingreso el numero 3");
+    case "3":
+        alert("El numero 3 corresponde al dia Miercoles");
         break;
-    case 4:
-        alert("Usted ingreso el numero 4");
+    case "4":
+        alert("El dia 4 corresponde al dia Jueves");
         break;
-        default: alert("No ingreso uno de los numeros pedidos")
+    case "5":
+        alert("El numero 5 corresponde al dia viernes");
+        break;
+    case "6":
+        alert("El numero 6 corresponde al dia Sabado");
+        break;
+    case "7":
+        alert("El dia 7 corresponde al dia Domingo")
+        break
+    default:
+        alert("No ingresaste un numero del 1 al 7")
 }
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
